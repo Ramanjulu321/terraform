52573b1f5e87002333bf835d969d099c1444127c
Ramanjulu1234 <ramanjulu7890@gmail.com>
2024-05-09 21:27:46 +0530

    terraform files

---
+++ conditions/ec2.tf
@@ -0,0 +1,9 @@
+# resource <resource-type> <resource-name>
+resource "aws_instance" "db" {
+
+    ami = var.image_id
+    instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"
+
+    tags = var.tags
+       
+}
